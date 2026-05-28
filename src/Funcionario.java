6bdb4fd1b9dcb45418099939a6ccfcdd59379003
fabriciobmachado <fabricio.b.machado1999@gmail.com>
2026-05-28 09:49:45 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,21 +1,21 @@
 public abstract class Funcionario {
-    // Atributos privados protegidos
+   
     private String nome;
     private int idade;
     private double salario;
 
-    // Construtor Coringa
+    
     public Funcionario() {
     }
 
-    // Construtor Completo
+    
     public Funcionario(String nome, int idade, double salario) {
         this.nome = nome;
         this.idade = idade;
         this.salario = salario;
     }
 
-    // Métodos Getters e Setters
+   
     public String getNome() {
         return this.nome;
     }
@@ -28,11 +28,11 @@ public abstract class Funcionario {
         }
     }
 
-    // Método de ação padrão
+    
     public void exibirRelatorio() {
         System.out.print("Colaborador: " + this.nome + " | Salário: R$ " + this.salario + " | Idade: " + this.idade + " anos ");
     }
 
-    // MÉTODO ABSTRATO: Sem corpo {}, obriga os filhos a criarem suas regras de bônus
+    
     public abstract void calcularBonus();
 }
