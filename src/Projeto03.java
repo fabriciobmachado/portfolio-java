10d24f735a748383f56bc799b56a36bda89d26c0
fabriciobmachado <fabricio.b.machado1999@gmail.com>
2026-05-28 09:53:15 -0300

    Update Projeto03.java

--- src/Projeto03.java
+++ src/Projeto03.java
@@ -8,13 +8,12 @@ public class Projeto03 {
 
         while (id <=60 ) {
 
-            // Se o ID for par, nós imprimimos ele
+            
             if (id % 3 == 0) {
                 System.out.println("Transação PAR encontrada - ID: " + id);
             }
 
-            // O id++ fica AQUI, no final do while, livre.
-            // Ele vai somar +1 SEMPRE, não importa se o número foi par ou ímpar.
+            
             id++;
         }
 
